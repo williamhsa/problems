f3ac6624c9f4744858991a735335d7a72ce25839
williamhsa <whsantonio@gmail.com>
2021-05-16 17:24:46 -0300

    Two Sum

---
+++ TwoSum/app.ts
@@ -0,0 +1,18 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+
+function twoSum(nums: number[], target: number): number[] {
+  
+};
+
+const inArray: number[] = [3,2,3]
+const inTarget: number = 6;
+
+const result = twoSum(inArray, inTarget);
+console.log("🚀 ~ file: app.ts ~ line 19 ~ result", result)
---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+function twoSum(nums, target) {
+}
+;
+const inArray = [3, 2, 3];
+const inTarget = 6;
+const result = twoSum(inArray, inTarget);
+console.log("🚀 ~ file: app.ts ~ line 19 ~ result", result);
